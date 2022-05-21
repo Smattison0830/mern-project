ba329fcd4a2ea831a3a3cfc0dddd3c240cca73b6
Spencer Mattison <spenceramattison@gmail.com>
2022-05-21 12:07:29 +0900

    Add css for UsersList.css and UserItem.css, Add shared components

--- src/users/components/UserItem.js
+++ src/users/components/UserItem.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React from 'react';
 
-import './UserItems.css'
+import './UserItem.css';
 
-export default UserItems( props ) {
-
-};
+export default function UserItems(props) {
+  return (
+    <li className="user-item">
+      <div className="user-item__content">
+        <div className="user-item__image">
+          <img src={props.image} alt={props.name} />
+        </div>
+        <div className="user-item__info">
+          <h2>{props.name}</h2>
+          <h3>
+            {props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}
+          </h3>
+        </div>
+      </div>
+    </li>
+  );
+}
