c009c401eeaebd8d37c20d4490ece1e0e99bfee6
Spencer Mattison <spenceramattison@gmail.com>
2022-05-21 19:13:08 +0900

    Create Navigation folder and needed components

---
+++ src/shared/components/Navigation/MainNavigation.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import MainHeader from './MainHeader';
+import './MainNavigation.css';
+
+export default function MainNavigation(props) {
+  return (
+    <MainHeader className="main-navigation__menu-btn">
+      <button>
+        <span />
+        <span />
+        <span />
+      </button>
+      <h1 className="main-navigation__title">
+        <Link to="/">Your Places</Link>
+      </h1>
+      <nav>...</nav>
+    </MainHeader>
+  );
+}
