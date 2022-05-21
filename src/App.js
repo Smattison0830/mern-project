d1da293669881f8e65c1c79be1a64d84e167b25e
Spencer Mattison <spenceramattison@gmail.com>
2022-05-21 19:13:08 +0900

    Create Navigation folder and needed components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 
-import Users from "./users/pages/Users";
-import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+import Users from './users/pages/Users';
+import NewPlace from './places/pages/NewPlace';
 
 export default function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/places/new" exact>
-          <NewPlace />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/places/new" exact>
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
 }
