7d10eee2db75964501edc2d99d4178e2f14c4d32
Spencer Mattison <spenceramattison@gmail.com>
2022-06-01 22:44:20 +0900

    Create validator for NewPlace.js

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
+import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
 import Input from '../../shared/components/FormElements/Input';
 import './NewPlace.css';
 
 export default function NewPlace() {
   return (
     <form className="place-form">
-      <Input type="text" label="Title" />
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[]}
+        errorText="Please enter a valid title."
+      />
     </form>
   );
 }
