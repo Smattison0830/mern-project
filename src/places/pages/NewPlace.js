4a7ef97ba73353d9b04343ae4a0842b51ba4bc87
Spencer Mattison <spenceramattison@gmail.com>
2022-06-02 18:38:52 +0900

    Add validator for forms in NewPlace.js

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,16 +1,60 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useReducer } from 'react';
 
-import {
-  VALIDATOR_MINLENGTH,
-  VALIDATOR_REQUIRE,
-} from '../../shared/util/validators';
 import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH,
+} from '../../shared/util/validators';
 import './NewPlace.css';
 
-export default function NewPlace() {
-  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case 'INPUT_CHANGE':
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        if (inputId === action.inputId) {
+          formIsValid = formIsValid && action.isValid;
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.inputId]: { value: action.value, isValid: action.isValid },
+        },
+        isValid: formIsValid,
+      };
+    default:
+      return state;
+  }
+};
 
-  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+export default function NewPlace() {
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: '',
+        isValid: false,
+      },
+      description: {
+        value: '',
+        isValid: false,
+      },
+    },
+    isValid: false,
+  });
+
+  const inputHandler = useCallback((id, value, isValid) => {
+    dispatch({
+      type: 'INPUT_CHANGE',
+      value: value,
+      isValid: isValid,
+      inputId: id,
+    });
+  }, []);
 
   return (
     <form className="place-form">
@@ -21,16 +65,19 @@ export default function NewPlace() {
         label="Title"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title."
-        onInput={titleInputHandler}
+        onInput={inputHandler}
       />
       <Input
         id="description"
         element="textarea"
         label="Description"
-        validators={VALIDATOR_MINLENGTH(5)}
+        validators={[VALIDATOR_MINLENGTH(5)]}
         errorText="Please enter a description (at least 5 characters)."
-        onInput={descriptionInputHandler}
+        onInput={inputHandler}
       />
+      <Button type="submit" disabled={!formState.isValid}>
+        SUBMIT
+      </Button>
     </form>
   );
 }
