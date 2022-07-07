19a1bd74f6711548b0e0f7611ca09b9ccbc437c8
Spencer Mattison <spenceramattison@gmail.com>
2022-07-07 13:44:26 +0900

    fixed error

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import UsersList from '../components/UsersList';
 import ErrorModal from '../../shared/components/UIElements/ErrorModal';
 import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import { useHttpClient } from '../../shared/hooks/http-hook';
 
 export default function Users() {
   const { isLoading, error, sendRequest, clearError } = useHttpClient();
@@ -11,7 +12,7 @@ export default function Users() {
     const fetchUsers = async () => {
       try {
         const responseData = await sendRequest(
-          'http://localhost:5000/api/users'
+          'http://localhost:5001/api/users'
         );
 
         setLoadedUsers(responseData.users);
